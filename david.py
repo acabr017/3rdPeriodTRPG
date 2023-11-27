5f703cfa35542ba34ff1d8ffe0c5d196ae9ca740
Dmah4980 <150825465+Dmah4980@users.noreply.github.com>
2023-11-27 09:02:38 -0500

    Update david.py

--- david.py
+++ david.py
@@ -1,1 +1,28 @@
 # This is where David will provide his parts of the project.
+
+Map of dungeon
+#player begings in A3
+_________
+| | | | |  d
+| | | | | c 
+| | | | | b
+| | | | | a
+`````````
+1 2 3 4 5
+
+Description='Descrip'
+Examination='Examine'
+Solved=False
+Up='North', 'up'
+down='South','down'
+left='West','Left'
+East='East','Right'
+
+Searched_spots= {'a1':False,'a2':False 'a3':False
+                'a1':False,'a2':False 'a3':False
+                'a1':False,'a2':False 'a3':False
+                'a1':False,'a2':False 'a3':False
+                }
+     
+
+Dungeon_Map= {

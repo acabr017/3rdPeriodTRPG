4bb052e96d75cb223bbfa1f1d20e0305788088ab
TheRealAlex707 <150163643+TheRealAlex707@users.noreply.github.com>
2023-12-02 17:48:37 -0500

    Update alejandro.py
    i updated it!!!

--- alejandro.py
+++ alejandro.py
@@ -14,30 +14,34 @@ class items:
               Type: {self.type} \n \
               Rarity: {self.rar} \n \
             "
-class weapon(items):
+class weapon(items): #ranged and melee can be in the same class, just that melee would have higher accuracy to hit
   def __init__(self, name, type, rarity, damage, accuracy, elemental):
     super().__init__(name, type, rarity)
     self.type = "weapon"
     self.dmg = damage
     self.acc = accuracy
     self.ele = elemental
-
-class potion(items):
+    #potential melee weapon ideas: rusty sword (starting weapon), basic sword, longsword, rapier, axe, dual axe, fire sword(?), spear, poison tipped sword or spear(?), super cool omega gamer blade of greatness (strongest sword in game)
+    #potential ranged weapon ideas: old bow (starting weapon), basic bow, crossbow, dynamite(?), poison darts, throwing knives, gun (final ranged weapon)
+class potion(items): #there are 2 main types of potions: 1 for healing and 1 for mana. There'll be mini potions that grant only half the boost of a normal potion.
   def __init__(self, name, type, rarity, health_boost, mana_boost)
   super().__init__(name, type, rarity)
   self.type = "potion"
   self.hb = health_boost
   self.mb = mana_boost
 
-class armor(items):
+class armor(items): #armor can be equipped and grants the player a defense boost. could grant an attack boost?
   def __init__(self, name, type, rarity, defense)
   super().__init__(name, type, rarity)
   self.type = "armor"
   self.def = defense
-  
-class spell(items):
+  #armor:rusty chainmail, chainmail, knight armor, palladin armor, etc...............
+class spell(items): #like ranged weapons, spells will have a decent chance to miss.
   def __init__(self, name, type, rarity, damage, mana_cost, ):
   super().__init__(name, type, rarity)
   self.type = spell
   self.damage = damage
+  self.acc = accuracy
+  self.ele = elemental
   self.mana_cost = mana_cost
+  #spells: fire blast, fireball, ice spike, freeze, poison
